f97afa79bfb4aaf9370b39dd404bf53953a56459
AnkitKumar8080 <ankitkumar44.80.80@gmail.com>
2024-07-26 10:07:33 +0530

    added vercel analytics

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,10 +6,12 @@ import ThemeContextWrapper from "./context/ThemeContextWrapper.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <SpeedInsights />
+    <Analytics />
     <BrowserRouter>
       <ThemeContextWrapper>
         <AuthProvider>
